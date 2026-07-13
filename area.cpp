c65c4991d8a522c15ccb4f47ae68542422ec40c9
srushtipatil1602 <srushtipatil922@gmail.com>
2026-07-13 10:52:14 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter a:";
+cin>>a;
+cout<<"enter b:";
+cin>>b;
+cout<<"area of rectangle:"<<a*b;
+cout<<"\nperimeter of rectangle:"<<2*(a+b);
+}
---
+++ areac.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a=3.14,r;
+cout<<"enter r:";
+cin>>r;
+cout<<"area of circle:"<<a*r*r;
+cout<<"\ncircumference of circle:"<<2*a*r;
+}
