5715dcbb3811ca0f38804391b850171dc651d0c2
srushtipatil1602 <srushtipatil922@gmail.com>
2026-07-13 10:54:15 +0530

    Add files via upload

---
+++ c3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cin>>a>>b;
+cout<<"addition:"<<a+b;
+cout<<"\nsub:"<<a-b;
+cout<<"\nmul:"<<a*b;
+cout<<"\ndiv:"<<a/b;
+cout<<"\nmod:"<<a%b;
+} 
