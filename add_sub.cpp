8a411594b0ce2c57526b3614986dae5e733f0e78
srushtipatil1602 <srushtipatil922@gmail.com>
2026-08-10 04:08:02 +0000

    changes

---
+++ add_sub.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+public:
+    float a, b, addition, subtraction;
+
+    // Constructor
+    student()
+    {
+        cout << "Enter two numbers: ";
+        cin >> a >> b;
+    }
+
+    void add()
+    {
+        addition = a + b;
+    }
+
+    void sub()
+    {
+        subtraction = a - b;
+    }
+
+    void print()
+    {
+        cout << "Addition = " << addition << endl;
+        cout << "Subtraction = " << subtraction << endl;
+    }
+};
+
+int main()
+{
+    student s;
+    s.add();
+    s.sub();
+    s.print();
+
+    return 0;
+}
